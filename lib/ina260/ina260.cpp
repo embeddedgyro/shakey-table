d10b24bf3b8a6709f6cd33fbed18100eacc11694
dalvador2 <torin@storkey.org>
2024-03-31 14:48:44 +0100

    ina initial development

---
+++ lib/ina260/ina260.cpp
@@ -0,0 +1,72 @@
+
+
+#include "ina260.h"
+#include <gpiod.hpp>
+
+namespace INA260_Driver {
+INA260::INA260(I2C_Interface *comInterface, INA260Interface *inaInterface) {
+  if (comInterface)
+    this->i2c = comInterface;
+  if (inaInterface)
+    this->ina260cb = inaInterface;
+}
+
+i2c_status_t INA260::InitializeSensor(Alert_Conf alert_mode,
+                                      Conv_Time volt_conv_time,
+                                      Conv_Time curr_conv_time,
+                                      Ave_Mode averaging_mode,
+                                      Op_Mode operating_mode) {
+  i2c_status_t result = AlertSet(alert_mode);
+
+  if (result == I2C_STATUS_SUCCESS)
+    result = CurrentConvTime(curr_conv_time);
+
+  if (result == I2C_STATUS_SUCCESS)
+    result = VoltageConvTime(volt_conv_time);
+
+  if (result == I2C_STATUS_SUCCESS)
+    result = AveragingMode(averaging_mode);
+
+  if (result == I2C_STATUS_SUCCESS)
+    result = OperatingMode(operating_mode);
+  return result;
+}
+
+void INA260::begin(void) {
+  dataAquisitionRunning = true;
+  dataAquisitionThread = std::thread(&INA260::dataAquisition, this);
+}
+
+void INA260::end(void) {
+  dataAquisitionRunning = false;
+  dataAquisitionThread.join();
+}
+
+void INA260::dataAquisition(void) {
+  const std::filesystem::path chip_path("/dev/gpiochip0");
+  const gpiod::line::offset line_offset = 5;
+
+  auto request = gpiod::chip(chip_path)
+                     .prepare_request()
+                     .set_customer("watch-line-value")
+                     .add_line_setting(
+                         line_offset, gpiod::line_settings()
+                                          .set_direction(gpiod::line
+                                                         : direction::INPUT)
+                                          .set_edge_detection(gpiod::line::endge
+                                                              : RISING))
+                     .do_request();
+  gpiod::edge_event_buffer buffer(1);
+
+  INA260Sample sample;
+
+  while (dataAquisitionRunning) {
+    sample.current = ReadCurrent(nullptr);
+    sample.voltage = ReadVoltage(nullptr);
+
+    ina260cb->hasSample(sample);
+
+    request.read_edge_events(buffer);
+  }
+}
+} // namespace INA260_Driver
--- lib/ina260/ina260.h
+++ lib/ina260/ina260.h
@@ -4,80 +4,120 @@
 #include <thread>
 
 namespace INA260_Driver {
-  enum class Alert_Conf{
-    OCL = 0b1000000000000000,
-    UCL = 0b0100000000000000,
-    BOL = 0b0010000000000000,
-    BUL = 0b0001000000000000
-    POL = 0b0000100000000000,
-    CNVR = 0b0000010000000000
-  };
-  enum class Ave_Mode{
-    AV1 = 0,
-    AV4 = 1,
-    AV16 = 2,
-    AV64 = 3,
-    AV128 = 4,
-    AV256 = 5,
-    AV512 = 6,
-    AV1024 = 7
-  };
-  
-  enum class Conv_Time{
-    TU140 = 0,
-    TU204 = 1,
-    TU332 = 2,
-    TU588 = 3,
-    TU1100 = 4,
-    TU2116 = 5,
-    TU4156 = 6,
-    TU8224 = 7
-  };
-  
-  enum class Op_Mode{
-    PDTRIG = 0,
-    CURTRIT = 1,
-    VOLTRIG = 2,
-    CURVOLTRIG = 3,
-    PDCONT = 4,
-    CURCONT = 5,
-    VOLCONT = 6,
-    CURVOLTRIG = 7
-  };
-  struct INA260Sample {
-    float current = 0;
-  };
+
+#define INA260_ADDRESS 0b1000000
+#define SensorConst static constexpr uint8_t
+
+namespace Sensor_Regs {
+SensorConst CONF_REG = 0x00;
+SensorConst CURRENT_REG = 0x01;
+SensorConst VOLTAGE_REG = 0x02;
+SensorConst POWER_REG = 0x03;
+SensorConst MASKEN_REG = 0x06;
+SensorConst ALERT_LIM = 0x07;
+SensorConst MAN_ID = 0xfe;
+SensorConst DIE_ID = 0xff;
+}; // namespace Sensor_Regs
+enum class Alert_Conf {
+  OCL = 0b1000000000000000,
+  UCL = 0b0100000000000000,
+  BOL = 0b0010000000000000,
+  BUL = 0b0001000000000000,
+  POL = 0b0000100000000000,
+  CNVR = 0b0000010000000000
+};
+enum class Ave_Mode {
+  AV1 = 0,
+  AV4 = 1,
+  AV16 = 2,
+  AV64 = 3,
+  AV128 = 4,
+  AV256 = 5,
+  AV512 = 6,
+  AV1024 = 7
+};
+
+enum class Conv_Time {
+  TU140 = 0,
+  TU204 = 1,
+  TU332 = 2,
+  TU588 = 3,
+  TU1100 = 4,
+  TU2116 = 5,
+  TU4156 = 6,
+  TU8224 = 7
+};
+
+enum class Op_Mode {
+  PDTRIG = 0,
+  CURTRIT = 1,
+  VOLTRIG = 2,
+  CURVOLTRIG = 3,
+  PDCONT = 4,
+  CURCONT = 5,
+  VOLCONT = 6,
+  CURVOLCONT = 7
+};
+struct INA260Sample {
+  float current = 0;
+  float voltage = 0;
+};
+/**
+ * @brief  Callback Interface where the callback needs to be
+ * implemented by the host application.
+ */
+class INA260Interface {
+public:
   /**
-  * @brief  Callback Interface where the callback needs to be 
-* implemented by the host application.
-  */
-  class INA260Interface {
-  public:
-    /**
-    * @brief Called after a sample has arrived.
-    */
-    virual void hasSample(INA260Sample& sample) = 0;
+   * @brief Called after a sample has arrived.
+   */
+  virtual void hasSample(INA260Sample &sample) = 0;
+};
+class INA260 {
+public:
+  INA260(I2C_Interface *comInterface, INA260Interface *inaInterface);
 
-  };
-  class INA260
-  {
-  public:
-    INA260(I2C_Interface* comInterface, INA260Interface* inaInterface);
-    
-    i2c_status_t InitializeSensor(Alert_conf init_setting = Alert_conf::CNVR);
-    
-    i2c_Status_t AveragingMode(Ave_Mode ave_setting = Ave_Mode::AV1);
-    
-    i2c_Status_t CurrentConvTime(Conv_Time convert_time = Conv_Time::TU140);
+  i2c_status_t InitializeSensor(Alert_Conf alert_mode = Alert_Conf::CNVR,
+                                Conv_Time volt_conv_time = Conv_Time::TU140,
+                                Conv_Time curr_conv_time = Conv_Time::TU140,
+                                Ave_Mode averaging_mode = Ave_Mode::AV1,
+                                Op_Mode operating_mode = Op_Mode::CURVOLCONT);
 
-    i2c_Status_t VoltageConvTime(Conv_Time convert_time = Conv_Time::TU140);
-    
-    i2c_Status_t OperatingMode(Op_Mode operate_mode = Op_Mode::PDTRIG);
-    
-    float ReadCurrent(i2c_Status_t* error)
+  void begin(void);
 
-    float ReadVoltage(i2c_Status_t* error)
-  }
-}
+  void end(void);
+
+  ~INA260() { end(); }
+
+  i2c_status_t AveragingMode(Ave_Mode ave_setting = Ave_Mode::AV1);
+
+  i2c_status_t CurrentConvTime(Conv_Time convert_time = Conv_Time::TU140);
+
+  i2c_status_t VoltageConvTime(Conv_Time convert_time = Conv_Time::TU140);
+
+  i2c_status_t OperatingMode(Op_Mode operate_mode = Op_Mode::PDTRIG);
+
+  i2c_status_t AlertSet(Alert_Conf alert_mode = Alert_Conf::CNVR);
+
+  float ReadCurrent(i2c_status_t *error = nullptr);
+
+  float ReadVoltage(i2c_status_t *error = nullptr);
+
+private:
+  /** Pointer to registered I2C interface. */
+  I2C_Interface *i2c = nullptr;
+
+  /** Pointer to registered MPU6050 interface. */
+  INA260Interface *ina260cb = nullptr;
+
+  /** Data aquisition thread. */
+  std::thread dataAquisitionThread;
+
+  /** Data aquisition flag. */
+  bool dataAquisitionRunning;
+
+  void dataAquisition(void);
+};
+} // namespace INA260_Driver
 
 #endif
