03035b587d959986cd921c814157dad0d533eacb
Jambulingam <jagandhanasekar@gmail.com>
2024-04-03 17:58:57 +0100

    Fixed issues in instance creation parameters

--- test/MPU6050/MPU6050UnitTest.cpp
+++ test/MPU6050/MPU6050UnitTest.cpp
@@ -3,11 +3,11 @@
 #include "MPU6050.h"
 
 // Test case for SetAccel_X_Offset method
-void testSetAccelXOffset() {
-    MPU6050 mpu6050;
+void testSetAccelXOffset(const MPU6050& mpu) {
+    //MPU6050 mpu6050;
 
     // Call the method
-    i2c_status_t result = mpu6050.SetAccel_X_Offset(100);
+    i2c_status_t result = mpu.SetAccel_X_Offset(100);
 
     // Verify the result
     if (result != I2C_STATUS_SUCCESS) {
@@ -16,12 +16,12 @@ void testSetAccelXOffset() {
 }
 
 // Test case for GetAccel_X_Offset method
-void testGetAccelXOffset() {
-    MPU6050 mpu6050;
+void testGetAccelXOffset(const MPU6050& mpu) {
+    //MPU6050 mpu6050;
 
     // Call the method
     i2c_status_t error;
-    int16_t offset = mpu6050.GetAccel_X_Offset(&error);
+    int16_t offset = mpu.GetAccel_X_Offset(&error);
 
     // Verify the result
     if (error != I2C_STATUS_SUCCESS || offset != 100) {
@@ -32,16 +32,18 @@ void testGetAccelXOffset() {
 int main() {
 
     I2C_Interface i2cInterface();
-    MPU6050 mpu6050(&i2cInterface);
+    MPU6050Interface mpu6050Interface();
+
+    MPU6050 mpu6050(&i2cInterface, &mpu6050Interface);
 
     // Initialize the sensor with desired parameters
     i2c_status_t initStatus = mpu6050.InitializeSensor(
         Gyro_FS_t::FULL_SCALE_250,
         Accel_FS_t::FULL_SCALE_2G,
         DLPF_t::BW_260Hz,
-        0, // Sample rate divider
-        0, // Interrupt configuration
-        0  // Interrupt enable
+        uint8_t SRdiv = 7,  // Sample Rate = Gyroscope Output Rate / (1 + SRdiv)
+	    uint8_t INTconf = Regbits_INT_PIN_CFG::BIT_INT_RD_CLEAR,
+	    uint8_t INTenable = Regbits_INT_ENABLE::BIT_DATA_RDY_EN;
     );
 
     if (initStatus != I2C_STATUS_SUCCESS) {
@@ -49,10 +51,11 @@ int main() {
         throw std::runtime_error(" Initialization failure!");
         return 1;
     }
+
     try{
     //Execute test case
-        testSetAccelXOffset();
-        testGetAccelXOffset();
+        testSetAccelXOffset(mpu6050);
+        testGetAccelXOffset(mpu6050);
     std::cout << "All tests passed!" << std::endl;
     }
     catch (const std::exception& e) {
