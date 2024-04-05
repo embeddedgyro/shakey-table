9ce07d10eafaba08d6b5dcdf97c25f0606139e89
adam <adamenglebright@rocketmail.com>
2024-04-05 01:05:15 +0100

    Modify INA260 lib to use the big endian I2C word transfers, and fix the logic for setting the Mask/Enable register.

--- lib/ina260/ina260.cpp
+++ lib/ina260/ina260.cpp
@@ -72,70 +72,70 @@ void INA260::dataAquisition(void) {
 
 float INA260::ReadVoltage(void) {
   uint16_t voltage_data =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
-  i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
+  i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
   return ReadingBases::VOLTAGE * voltage_data;
 }
 
 float INA260::ReadCurrent(void) {
   uint16_t current_data =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
-  i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
+  i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
   return ReadingBases::CURRENT * current_data;
 }
 
 float INA260::ReadPower(void) {
   uint16_t power_data =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
-  i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::VOLTAGE_REG);
+  i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::MASKEN_REG);
   return ReadingBases::POWER * power_data;
 }
 
 i2c_status_t INA260::AlertSet(Alert_Conf alert_mode) {
   uint16_t alert_data;
-  alert_data = (uint8_t)alert_mode >> 8;
-  return i2c->WriteRegisterWord(INA260_ADDRESS, Sensor_Regs::MASKEN_REG,
+  alert_data = (uint16_t)alert_mode << 8;
+  return i2c->WriteRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::MASKEN_REG,
                                 alert_data);
 }
 
 i2c_status_t INA260::CurrentConvTime(Conv_Time convert_time) {
   uint16_t conv_time_reg =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG);
   uint16_t mask = ~0b0000000000111000;
   conv_time_reg = conv_time_reg & mask;
   conv_time_reg = conv_time_reg | ((uint16_t)convert_time << 3);
-  return i2c->WriteRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG,
+  return i2c->WriteRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG,
                                 conv_time_reg);
 }
 
 i2c_status_t INA260::VoltageConvTime(Conv_Time convert_time) {
   uint16_t conv_time_reg =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG);
   uint16_t mask = ~0b0000000111000000;
   conv_time_reg = conv_time_reg & mask;
   conv_time_reg = conv_time_reg | ((uint16_t)convert_time << 6);
-  return i2c->WriteRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG,
+  return i2c->WriteRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG,
                                 conv_time_reg);
 }
 
 i2c_status_t INA260::OperatingMode(Op_Mode operate_mode) {
   uint16_t conf_reg_data =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG);
   uint16_t mask = ~0b0000000000000111;
   conf_reg_data = conf_reg_data & mask;
   conf_reg_data = conf_reg_data | ((uint16_t)operate_mode);
   std::cout << "Data sent to config register: " << std::hex << conf_reg_data << std::endl;
-  return i2c->WriteRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG,
+  return i2c->WriteRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG,
                                 conf_reg_data);
 }
 
 i2c_status_t INA260::AveragingMode(Ave_Mode ave_setting) {
   uint16_t conf_reg_data =
-      i2c->ReadRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG);
+      i2c->ReadRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG);
   uint16_t mask = ~0b0000111000000000;
   conf_reg_data = conf_reg_data & mask;
   conf_reg_data = conf_reg_data | ((uint16_t)ave_setting << 9);
-  return i2c->WriteRegisterWord(INA260_ADDRESS, Sensor_Regs::CONF_REG,
+  return i2c->WriteRegisterWordBigEndian(INA260_ADDRESS, Sensor_Regs::CONF_REG,
                                 conf_reg_data);
 }
 
