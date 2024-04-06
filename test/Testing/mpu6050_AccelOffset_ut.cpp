285abc06cb3f8dc9a5d7b190c85e7ebcf2a1035a
Jambulingam <jagandhanasekar@gmail.com>
2024-04-06 16:10:53 +0100

    putting files together for online testing

---
+++ test/Testing/mpu6050_AccelOffset_ut.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cassert>
+#include "mpu6050.h"
+
+// Test case for SetAccel_X_Offset method
+void testSetAccelXOffset(const MPU6050& mpu) {
+    //MPU6050 mpu6050;
+
+    // Call the method
+    i2c_status_t result = mpu.SetAccel_X_Offset(100);
+
+    // Verify the result
+    if (result != I2C_STATUS_SUCCESS) {
+        throw std::runtime_error("SetAccelXOffset test failed!");
+}
+}
+
+// Test case for GetAccel_X_Offset method
+void testGetAccelXOffset(const MPU6050& mpu) {
+    //MPU6050 mpu6050;
+
+    // Call the method
+    i2c_status_t error;
+    int16_t offset = mpu.GetAccel_X_Offset(&error);
+
+    // Verify the result
+    if (error != I2C_STATUS_SUCCESS || offset != 100) {
+        throw std::runtime_error("GetAccelXOffset test failed!");
+    }
+}
+
+int main() {
+
+    I2C_Interface i2cInterface();
+    MPU6050Interface mpu6050Interface();
+
+    MPU6050 mpu6050(&i2cInterface, &mpu6050Interface);
+
+    // Initialize the sensor with desired parameters
+    i2c_status_t initStatus = mpu6050.InitializeSensor(
+        Gyro_FS_t::FULL_SCALE_250,
+        Accel_FS_t::FULL_SCALE_2G,
+        DLPF_t::BW_260Hz,
+        uint8_t SRdiv = 7,  // Sample Rate = Gyroscope Output Rate / (1 + SRdiv)
+	    uint8_t INTconf = Regbits_INT_PIN_CFG::BIT_INT_RD_CLEAR,
+	    uint8_t INTenable = Regbits_INT_ENABLE::BIT_DATA_RDY_EN;
+    );
+
+    if (initStatus != I2C_STATUS_SUCCESS) {
+        //Capturing initialization failure
+        throw std::runtime_error(" Initialization failure!");
+        return 1;
+    }
+
+    try{
+    //Execute test case
+        testSetAccelXOffset(mpu6050);
+        testGetAccelXOffset(mpu6050);
+    std::cout << "All tests passed!" << std::endl;
+    }
+    catch (const std::exception& e) {
+        //Capturing test case failure
+        std::cerr << "Test failed: " << e.what() << std::endl;
+        return 1; // Returning non-zero exit code to indicate test failure
+    }
+    return 0;
+}
